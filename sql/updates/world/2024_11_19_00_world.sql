a035bc1cd5de1d75e3d12d744dc84ce8c657e694
lee <leelf_00@126.com>
2024-11-21 23:12:24 +0800

    Add locale text

--- sql/updates/world/2024_11_19_00_world.sql
+++ sql/updates/world/2024_11_19_00_world.sql
@@ -9,3 +9,16 @@ INSERT INTO `spell_target_position` (`id`, `effIndex`, `target_map`, `target_pos
 
 -- Update wrong name and spellid of ancient-portal-dalaran
 UPDATE `gameobject_template` SET `name`='Portal to Dalaran',`data0`=121848 WHERE `entry`=211835;
+
+-- Add locale text
+DELETE FROM `gameobject_template_locale` WHERE `entry`=211835 AND `locale` IN ('deDE','esES','esMX','frFR','koKR','ruRU','zhCN','zhTW');
+INSERT INTO `gameobject_template_locale` (`entry`, `locale`, `name`, `castBarCaption`, `VerifiedBuild`) VALUES
+(211835, 'deDE', 'Portal nach Dalaran', '', 18019),
+(211835, 'esES', 'Portal a Dalaran', '', 18019),
+(211835, 'esMX', 'Portal a Dalaran', '', 18019),
+(211835, 'frFR', 'Portail vers Dalaran', '', 18019),
+(211835, 'koKR', '달라란으로 통하는 차원문', '', 18019),
+(211835, 'ruRU', 'Портал в Даларан', '', 18019),
+(211835, 'zhCN', '通往达拉然的传送门', '', 18019),
+(211835, 'zhTW', '達拉然傳送門', '', 18019);
+
